40651e879a59030c4336542393c3619f87ff6c67
Antonis Rusman <75878415+AntonisRsmn@users.noreply.github.com>
2024-10-09 05:20:30 +0300

    Update 09/10/2024 05:20

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ const themeSwitch = document.getElementById("theme-switch")
 const enableDarkmode = () => {
     document.body.classList.add("darkmode")
     localStorage.setItem("darkmode", "active")
-    document.getElementById("logo").src="../Imgs/logo-white.png"
+    // document.getElementById("logo").src="../Imgs/logo-white.png"
 }
 
 const disableDarkmode = () => {
     document.body.classList.remove("darkmode")
     localStorage.setItem("darkmode", null)
-    document.getElementById("logo").src="../Imgs/logo-blue.png";
+    // document.getElementById("logo").src="../Imgs/logo-blue.png";
 }
 
 if (darkmode ==="active") enableDarkmode()
@@ -31,4 +31,5 @@ themeSwitch.addEventListener("click", () => {
     darkmode = localStorage.getItem("darkmode")
 
     darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+    darkmode === "active" ? document.getElementById("logo").src="../Imgs/logo-blue.png" : document.getElementById("logo").src="../Imgs/logo-white.png";
 })
